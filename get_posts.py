ebbe9fa3e951b90912151540b5e804f08a21afb6
A1337Xyz <blindwizard@tutanota.com>
2022-12-23 23:46:48 -0300

    2022.12.23

--- get_posts.py
+++ get_posts.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from optparse import OptionParser
+from datetime import datetime
 from aiohttp import ClientSession, BasicAuth
 from html import unescape
 import asyncio
@@ -8,27 +8,16 @@ import os
 import random
 import re
 
-parser = OptionParser()
-parser.add_option('-p', '--page',     type='int',    default=1)
-parser.add_option('-m', '--max-page', type='int',    default=None)
-parser.add_option('-l', '--limit',    type='int',    default=100)
-parser.add_option('-s', '--sort',     type='string', default='modified')
-parser.add_option('-o', '--order',    type='string', default='desc')
-opts, args = parser.parse_args()
-assert opts.page >= 1
-assert opts.order in ['asc', 'desc']
-assert opts.sort in [
-    'author', 'date', 'id', 'modified',
-    'relevance', 'slug', 'title', 'rand'
-]
+# orderby = author, date, id, modified, relevance, slug, title, rand
 
 USER = '' # anitsu login user and password
 PASS = ''
 HOME = os.getenv('HOME')
+ROOT = os.path.realpath(os.path.dirname(__file__))
+LAST_RUN = os.path.join(ROOT, '.last_run')
 DB = os.path.join(HOME, '.cache/anitsu.json')
 IMG_DIR = os.path.join(HOME, '.cache/anitsu_covers')
-API_URL =  'https://anitsu.moe/wp-json/wp/v2/posts'
-API_URL += '?per_page={}&page={}&orderby={}&order={}&_fields=id,date,modified,link,title,content'
+WP_URL =  'https://anitsu.moe/wp-json/wp/v2/posts?per_page=100&page={}&modified_after={}&_fields=id,date,modified,link,title,content'
 RE_IMG  = re.compile(r'src=\"([^\"]*\.(?:png|jpe?g|webp|gif))')
 RE_MAL  = re.compile(r'myanimelist\.net/\w*/(\d*)')
 RE_ANI  = re.compile(r'anilist\.co/\w*/(\d*)')
@@ -36,6 +25,7 @@ RE_GDR  = re.compile(r'href=\"(https://drive\.google[^\"]*)')
 RE_PASS = re.compile(r'Senha: <span[^>]*>(.*)</span')
 MAX_ATTEMPTS = 5
 Q_SIZE = 10
+NOW = datetime.isoformat(datetime.now())
 
 
 clean_text = lambda s: unescape(s).encode('ascii', 'ignore').decode().strip()
@@ -102,7 +92,7 @@ async def update_db(posts):
 async def get_posts(queue):
     while True:
         page = await queue.get()
-        url = API_URL.format(opts.limit, page, opts.sort, opts.order)
+        url = WP_URL.format(page, last_run)
         att = 0
         while att < MAX_ATTEMPTS:
             try:
@@ -120,38 +110,46 @@ async def get_posts(queue):
 
 
 async def main():
-    global session, db, qsize
+    global session, db, qsize, last_run
     try:
         with open(DB, 'r') as fp:
             db = json.load(fp)
     except FileNotFoundError:
         db = dict()
 
+    try:
+        with open(LAST_RUN, 'r') as fp:
+            last_run = fp.read()
+    except FileNotFoundError:
+        last_run = NOW
+
+    open(LAST_RUN, 'w').write(NOW)
+
     auth = BasicAuth(USER, PASS)
     async with ClientSession(auth=auth) as session:
         print('requesting first page, please wait...')
-        url = API_URL.format(opts.limit, opts.page, opts.sort, opts.order)
+        url = WP_URL.format(1, last_run)
         async with session.get(url) as r:
             total_pages = int(r.headers['x-wp-totalpages'])
             total_posts = int(r.headers['x-wp-total'])
             posts = await r.json()
+
         if not posts:
             return
+
         print(f'total pages: {total_pages}\ntotal posts: {total_posts}')
         await update_db(posts)
-        total_pages = opts.max_page if opts.max_page else total_pages
-        if opts.page < total_pages:
-            queue = asyncio.Queue()
-            for p in range(opts.page + 1, total_pages+1):
-                queue.put_nowait(p)
-            qsize = queue.qsize()
-            tasks = []
-            for _ in range(Q_SIZE):
-                tasks += [asyncio.create_task(get_posts(queue))]
-            await queue.join()
-            for task in tasks:
-                task.cancel()
-            await asyncio.gather(*tasks, return_exceptions=True)
+        queue = asyncio.Queue()
+        for p in range(2, total_pages+1):
+            queue.put_nowait(p)
+        qsize = queue.qsize()
+        tasks = []
+        for _ in range(Q_SIZE):
+            tasks += [asyncio.create_task(get_posts(queue))]
+        await queue.join()
+        for task in tasks:
+            task.cancel()
+        await asyncio.gather(*tasks, return_exceptions=True)
 
     with open(DB, 'w') as fp:
         json.dump(db, fp)
